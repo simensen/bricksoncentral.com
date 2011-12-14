665a14a160a5e0a24ceb8e65b500dcceb34dc63c
Beau Simensen <beau@dflydev.com>
2011-12-13 23:00:36 -0600

    Initial work.

---
+++ README.markdown
@@ -0,0 +1,6 @@
+Bricks on Central Website
+=========================
+
+This repository contains (almost) everything that makes up the [Bricks on Central](http://bricksoncentral.com/) website.
+
+&copy; Beau Simensen
---
+++ js/script.js
@@ -0,0 +1,62 @@
+(function($) {
+
+    var retina = false;
+    if(Modernizr.mq('only screen and (-webkit-min-device-pixel-ratio: 2)')) {
+        retina = true;
+    }
+
+    var images = $('#content .building-photos img').get();
+
+    var updateImages = function() {
+
+        var updates = [];
+        $(images).each(function() {
+            var $image = $(this);
+            var src, width, height;
+            if (Modernizr.mq('only screen and (min-width: 768px)')) {
+                width = $image.attr('width-768');
+                height = $image.attr('height-768');
+                src = retina ? $image.attr('src-768x2') : $image.attr('src-768');
+            } else if (Modernizr.mq('only screen and (min-width: 480px)')) {
+                width = $image.attr('width-480');
+                height = $image.attr('height-480');
+                src = retina ? $image.attr('src-480x2') : $image.attr('src-480');
+            } else {
+                // lower source images
+                width = $image.attr('width-320');
+                height = $image.attr('height-320');
+                src = retina ? $image.attr('src-320x2') : $image.attr('src-320');
+            }
+            if ( src != $image.attr('src') ) {
+                if ( width < $image.attr('width') ) {
+                    // We should do nothing here.
+                } else {
+                    updates.push([$image, src, width, height]);
+                }
+            }
+        });
+        if ( updates.length ) {
+            for ( var i = 0; i < updates.length; i++ ) {
+                var update = updates[i];
+                var $image = update[0];
+                $image.attr('src', update[1]);
+                $image.attr('width', update[2]);
+                $image.attr('height', update[3]);
+            }
+        }
+
+
+    }
+
+    updateImages();
+
+    var timer;
+
+    $(window).resize(function () {
+        if ( timer ) { clearTimeout(timer); }
+        timer = setTimeout(function() {
+            updateImages();
+        }, 2000);
+    });
+
+})(jQuery);
